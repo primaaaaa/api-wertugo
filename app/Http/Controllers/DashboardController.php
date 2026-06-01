1eab99361f1883d998dd89cf9a28609dca85a1f5
Primaaaaa <primayudhistira040@gmail.com>
2026-06-01 18:16:57 +0700

    FIX: total_verifikasi dan total_report di dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Account;
+use App\Models\Report;
+use App\Models\Verification;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,11 +12,15 @@ class DashboardController extends Controller
     public function index(){
         $totalUser = Account::where('role', 'user')->count();
         $totalUmkm = Account::where('role', 'umkm')->count();
+        $totalVerificationPending = Verification::where('verification_status', 'pending')->count();
+        $totalReportPending = Report::where('report_status', 'pending')->count();
         // $totalVerifikasi
 
         return response()->json([
             'total_user' => $totalUser,
-            'total_umkm' => $totalUmkm
+            'total_umkm' => $totalUmkm,
+            'total_verifikasi' => $totalVerificationPending,
+            'total_report' => $totalReportPending
         ]);
 
 
